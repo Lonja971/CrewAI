751da1765c469b34ca23e9b1a1b1ea6016908ca8
lonja971 <leonidalekseev59@gmail.com>
2024-09-18 16:19:44 +0200

    Delete

--- project2/setup/create-llama2-model-file.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-model_name="llama2"
-custom_model_name="crewai-llama2"
-
-ollama pull "$model_name"
-
-ollama create "$custom_model_name" -f ./setup/Llama2Modelfile
--- project2/setup/create-mistral-model-file.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-model_name="mistral"
-custom_model_name="crewai-mistral"
-
-ollama pull "$model_name"
-
-ollama create "$custom_model_name" -f ./setup/MistralModelfile
